1211651b7b7eac8bd2aba81ad6dd36c922b27bd1
Vinícius Marques <vini.type@gmail.com>
2020-05-22 21:49:33 -0300

    style: refactor code after tslint

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+
 import { AppController } from './api/v1/AppController'
 import { AppService } from './services/AppService'
 
 @Module({
     imports: [],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './AppModule'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     await app.listen(3000)
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap()
