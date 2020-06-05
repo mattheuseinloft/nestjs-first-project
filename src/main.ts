d6ba91261786b3c996709d661a3c2cc4dd5fc1f3
Lucas <lucas.gusmao@roit.com.br>
2020-06-05 16:12:45 -0300

    adding environment and roit libs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
-
 import { AppModule } from './AppModule'
+import { Environment } from 'roit-environment';
+import { Logger } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    app.setGlobalPrefix('api/v1')
+    app.use(bodyParser.json({ limit: '10mb' }))
+    app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+    app.enableCors()
+    await app.listen(Environment.getProperty("port"))
+    Logger.log(`Listening on http://localhost:${Environment.getProperty("port")}/`)
+
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap()
