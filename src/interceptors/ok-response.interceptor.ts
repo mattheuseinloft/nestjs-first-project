6936884bbc0d9dfbaf8599fc18f38a613f2043a9
Lucas <lucas.gusmao@roit.com.br>
2020-06-15 11:10:52 -0300

    adding interceptor, changing okresponse lib

---
+++ src/interceptors/ok-response.interceptor.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { OkResponse } from "@roit/roit-response-handler";
+
+@Injectable()
+export class OkResponseInterceptor<T> implements NestInterceptor<T> {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(map(data => (OkResponse(data))));
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './AppModule'
 import { Environment } from 'roit-environment';
 import { Logger } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { OkResponseInterceptor } from './interceptors/ok-response.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -10,6 +11,7 @@ async function bootstrap() {
     app.use(bodyParser.json({ limit: '10mb' }))
     app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
     app.enableCors()
+    app.useGlobalInterceptors(new OkResponseInterceptor())
     await app.listen(Environment.getProperty("port"))
     Logger.log(`Listening on http://localhost:${Environment.getProperty("port")}/`)
 
